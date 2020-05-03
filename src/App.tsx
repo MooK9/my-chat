48afdba56a5e0c81c4e467b41127072f3a68961c
Bakos Ádám <drmook9@gmail.com>
2020-05-03 18:57:48 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Login} from "./login/Login";
+import { proxy } from './proxy/proxy';
+import { Main } from './components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component
+{
+    state = { showLogin: true };
+
+    componentDidMount()
+    {
+      proxy.addEventListener( "login", () => { this.setState( { showLogin: false } ) } );
+    }
+  
+    render()
+    {
+      return (
+        <div className="app">
+          { this.state.showLogin ? <Login /> : <Main /> }
+        </div>
+      );
+    }
+  
+
 }
-
-export default App;
---
+++ src/proxy/proxy.ts
@@ -0,0 +1,63 @@
+import {MessageDto, InboxDto, IncomingPacket, OutgoingPacket} from '../interfaces/chat';
+import { EventProducer } from './eventproducer';
+
+
+interface ProxyEventMap{
+    "login": () => void;
+    "message": ( channelId: string, message: MessageDto ) => void;
+    "conversation": ( channelId: string ) => void;
+}
+
+class Proxy extends EventProducer<ProxyEventMap>{
+    private ws: WebSocket;
+    inbox: InboxDto | null = null;
+    constructor()
+    {
+        super();
+        //TODO change this to aut address
+        this.ws = new WebSocket( "wss://raja.aut.bme.hu/chat/" );
+        this.ws.addEventListener( "open", () =>{
+            
+
+        });
+        this.ws.addEventListener( "message", e =>{
+    
+        });
+        
+        this.ws.addEventListener( "message", e =>{
+            let p = <IncomingPacket>JSON.parse( e.data );
+            switch ( p.type ){
+                case "error":
+                    alert( p.message );
+                     break;
+                case "login":
+                    this.inbox = p.inbox;
+                    this.dispatch( "login" );
+                    break;
+                case "message":
+                    let cid = p.channelId;
+                    this.inbox!.conversations.find( x => x.channelId === cid )?.lastMessages.push( p.message );
+                    this.dispatch( "message", cid, p.message );
+                     break;
+                case "conversationAdded":
+                    this.inbox!.conversations.push( p.conversation );
+                    this.dispatch( "conversation", p.conversation.channelId );
+                    break;
+                }
+        });
+
+
+    }
+
+    public sendPacket( packet: OutgoingPacket )
+    {
+    this.ws.send( JSON.stringify( packet ) );
+    }
+
+    
+}
+
+export var proxy = new Proxy();
+
+
+
