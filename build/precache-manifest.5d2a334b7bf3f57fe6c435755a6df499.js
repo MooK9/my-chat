896481b533796a0463f2ea84b5e64a28c6221e8e
Bakos Ádám <drmook9@gmail.com>
2020-05-03 18:59:02 +0200

    included build

---
+++ build/precache-manifest.5d2a334b7bf3f57fe6c435755a6df499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52a7a58638fa4e917103a349833d7fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "600c3fcbe5879558a074",
+    "url": "./static/css/main.1b9c5ada.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600c3fcbe5879558a074",
+    "url": "./static/js/main.bde9957e.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
